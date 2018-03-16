760511657a5c6c8b922cd76eec71493f72a5fdc9
Robin <robin.ryden@strateg.se>
2018-03-16 14:56:08 +0100

    Update Get() function for getting all accounts of user

--- accounts.go
+++ accounts.go
@@ -1,17 +1,17 @@
 package swedbank
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 var (
 	client   = &http.Client{}
-	accounts = "https://psd2.api.swedbank.com/sandbox/v1/accounts"
+	accounts = "https://psd2.api.swedbank.com/sandbox/v1/accounts/?bic=SANDSESS"
 )
 
 type Accounts struct {
-	BIC           string
 	WithBalance   bool
 	Date          string
 	Authorization string
@@ -19,20 +19,61 @@ type Accounts struct {
 	ProcessID     string
 }
 
+type Account struct {
+	ID             string               `json:"id"`
+	Currency       string               `json:"currency"`
+	Product        string               `json:"product"`
+	AccountType    string               `json:"account_type"`
+	IBAN           string               `json:"iban"`
+	BIC            string               `json:"bic"`
+	BBAN           string               `json:"bban"`
+	ClearingNumber string               `json:"clearingnumber"`
+	AccountNumber  string               `json:"account_number"`
+	Balances       []map[string]Balance `json:"balances"`
+}
+
+type AccountList struct {
+	List []Account `json:"account_list"`
+}
+
+type Balance struct {
+	Amount Amount `json:"amount"`
+	Date   string `json:"date"`
+}
+
+type Amount struct {
+	Currency string  `json:"currency"`
+	Content  float64 `json:"content"`
+}
+
 // This lists all payment accounts on the user
-func Get(acc *Accounts) int {
-	payload, err := http.Get(accounts)
-	payload.Header.Add("BIC", acc.BIC)
+func Get(acc *Accounts) (*AccountList, error) {
+	payload, err := http.NewRequest("GET", accounts, nil)
 	payload.Header.Add("Authorization", acc.Authorization)
 	payload.Header.Add("Process-ID", acc.ProcessID)
 	payload.Header.Add("Request-ID", acc.RequestID)
 	payload.Header.Add("Date", acc.Date)
 
+	req, err := client.Do(payload)
+
 	if err != nil {
 		fmt.Println("Error occured while trying to fetch from", accounts)
 	}
 
-	defer payload.Body.Close()
+	defer req.Body.Close()
 
-	return payload.StatusCode
+	decoder := json.NewDecoder(req.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	test := AccountList{}
+	err = decoder.Decode(&test)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &test, nil
 }
