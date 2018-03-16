bfe58abaec9adfad9c4cd0e2b880f70d9de3c95e
Robin <robin.ryden@strateg.se>
2018-03-16 21:07:12 +0100

    Update structs for TransactionList and Transaction

--- accounts.go
+++ accounts.go
@@ -61,10 +61,17 @@ type Amount struct {
 
 // Transactions
 type TransactionList struct {
-	List []Transaction `json:"transactions"`
+	List []map[string]Transaction `json:"transactions"`
 }
 
 type Transaction struct {
+	CreditDebit           string               `json:"credit_debit"`
+	Amount                Amount               `json:"amount"`
+	BookingDate           string               `json:"booking_date"`
+	TransactionDate       string               `json:"transaction_date"`
+	ValueDate             string               `json:"value_date"`
+	RemittanceInformation string               `json:"remittance_information"`
+	Balances              []map[string]Balance `json:"balances"`
 }
 
 // GetAccounts ...
@@ -132,6 +139,6 @@ func GetAccount(acc *SingleAccount) (*Account, error) {
 
 // GetTransactions ...
 // Fetch transactions from the account
-func GetTransactions() {
-
+func GetTransactions(acc *SingleAccount) (*TransactionList, error) {
+	return nil, nil
 }
