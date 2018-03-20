c4ee91086c492f11e412d6409e2122629e24389d
Robin <robin.ryden@strateg.se>
2018-03-20 23:23:16 +0100

    added GetTransactions() for fetching transaction lists from specific accounts

--- transactions.go
+++ transactions.go
@@ -1,8 +1,14 @@
 package swedbank
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 // Transactions
 type TransactionList struct {
-	List []map[string]Transaction `json:"transactions"`
+	List map[string][]Transaction `json:"transactions"`
 }
 
 type Transaction struct {
@@ -18,5 +24,32 @@ type Transaction struct {
 // GetTransactions ...
 // Fetch transactions from the account
 func GetTransactions(acc *SingleAccount) (*TransactionList, error) {
-	return nil, nil
+	payload, err := http.NewRequest("GET", accountAPI+acc.ID+"/transactions/?bic="+acc.BIC+"&date_from=2016-01-01&date_to=2018-01-01", nil)
+	payload.Header.Add("Process-ID", acc.ProcessID)
+	payload.Header.Add("Request-ID", acc.RequestID)
+	payload.Header.Add("Authorization", acc.Authorization)
+
+	req, err := client.Do(payload)
+
+	if err != nil {
+		fmt.Println("Error occured while trying to fetch from", accountAPI)
+	}
+
+	defer req.Body.Close()
+	decoder := json.NewDecoder(req.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(decoder)
+
+	transaction := TransactionList{}
+	err = decoder.Decode(&transaction)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &transaction, nil
 }
